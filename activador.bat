e7c7b044568996d72d803f6db9ac3791c7d891fe
Alvaro Gomez Suarez <85367600+alvarogmzsz@users.noreply.github.com>
2024-01-13 00:34:00 +0100

    Update activador.bat

--- activador.bat
+++ activador.bat
@@ -1,29 +1,21 @@
-:: Inicio Activaci�n.
+:: Inicio Activación.
 @echo off
-mode con cols=44 lines=9
-color 0e
-
-if not exist %SystemDrive%\Menu_Activacion\948375274093480983928.txt (goto activar) else (goto error)
+if not exist %SystemDrive%\Activacion\948375274093480983928.txt (goto activar) else (goto error)
 
 :activar
-mkdir %SystemDrive%\Menu_Activacion\
-echo 812361232137xnq218> %SystemDrive%\Menu_Activacion\948375274093480983928.txt
-attrib +h %SystemDrive%\Menu_Activacion\948375274093480983928.txt
+mkdir C:\Activacion\
+echo 98328yqwd89r983yr90wf0y509705709505709570750750975093ure093u0e9dxh94ohtehfoyu059095u050u5096u9u59u696> %SystemDrive%\Activacion\948375274093480983928.txt
+attrib +h %SystemDrive%\Activacion\948375274093480983928.txt
 cls
-echo.
-echo Activacion creada satisfactoriamente para
-echo %Username%
-echo.
-echo Presione ENTER para continuar
+echo Activacion Creada Satisfactoriamente Para %UserProfile%
+echo Presione ENTER para continuar.
 pause>nul
-exit
+goto eof
 
 :error
-echo.
-echo Lo sentimos, en este equipo el MENU ya est� activado
-echo.
-echo Presione ENTER para continuar
+msg * Lo sentimos, en este equipo ya esta activado el MENU.
+echo Presione ENTER para continuar.
 pause>nul
-exit
+goto eof
 
-:: Fin Activacio�n.
+:: Fin Activación.
